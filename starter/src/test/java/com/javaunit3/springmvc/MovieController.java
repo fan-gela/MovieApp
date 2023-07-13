38ff59b44dd54b4a6898581acec0204217f6c085
Angela <angela.fan@oracle.com>
2023-07-13 12:10:51 -0700

    website complete

--- starter/src/test/java/com/javaunit3/springmvc/MovieController.java
+++ starter/src/test/java/com/javaunit3/springmvc/MovieController.java
@@ -1,7 +1,10 @@
 package com.javaunit3.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MovieController {
@@ -9,4 +12,22 @@ public class MovieController {
     public String getIndexPage() {
         return "index";
     }
+    @RequestMapping("/bestMovie")
+    public String getBestMoviePage(Model model) {
+    model.addAttribute("BestMovie", bestMovieService.getBestMovie().getTitle());
+        return "bestMovie";
+    }
+    @RequestMapping("/voteForBestMovieForm")
+    public String voteForBestMovieFormPage() {
+        return "voteForBestMovie";
+    }
+    
+    @RequestMapping("/voteForBestMovie")
+    public String voteForBestMovie(HttpServletRequest request, Model model) {
+        String movieTitle = request.getParameter("movieTitle");
+        model.addAttribute("BestMovieVote", movieTitle);
+        return "voteForBestMovie";
+    }
 }
+
+
